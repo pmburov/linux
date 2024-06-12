1348288e07ca28a14fa008ea075b6cfd61503372
Pavel <you@example.com>
2024-06-12 20:47:26 +0300

    fixes

--- apps/obs.sh
+++ apps/obs.sh
@@ -1,4 +1,5 @@
 # --- OBS ---
-sudo add-apt-repository -y ppa:obsproject/obs-studio
-sudo apt update
-sudo apt install -y ffmpeg obs-studio
+# sudo add-apt-repository -y ppa:obsproject/obs-studio
+# sudo apt update
+# sudo apt install -y ffmpeg obs-studio
+flatpak install flathub com.obsproject.Studio
