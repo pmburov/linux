6eea0036ff81a5042d74752874b492e6272525db
Pavel <you@example.com>
2024-06-17 01:18:35 +0300

    cli for penpot

--- apps/optional/penpot/penpost.sh
+++ apps/optional/penpot/penpost.sh
@@ -1,7 +1,10 @@
-# --- PenPot ---
+# --- Penpot ---
 mkdir ~/.penpot
 cd ~/.penpot
 wget https://raw.githubusercontent.com/penpot/penpot/main/docker/images/docker-compose.yaml
-echo 'PenPot docker config installed'
+echo 'Penpot docker config installed'
 echo 'Installing docker images and stuff'
 sudo docker compose -p penpot -f docker-compose.yaml up -d
+gum spin --spinner dot --title "Waiting for containers to spin up" -- sleep 15
+xdg-open http://localhost:9001 > /dev/null
+cp ~/.linux/apps/optional/penpot/penpot.desktop ~/.local/share/applications/penpot.desktop
