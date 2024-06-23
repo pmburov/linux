9482a2495bbed126ac1df51ac51c6e56722a11bb
Pavel <you@example.com>
2024-06-24 01:02:14 +0300

    install eiciel balena-etcher

---
+++ apps/optional/balena-etcher.sh
@@ -0,0 +1,8 @@
+# --- balenaEtcher ---
+sudo apt install -y libfuse2
+# cd ~/Downloads
+# wget https://github.com/balena-io/etcher/releases/download/v1.19.21/balena-etcher_1.19.21_amd64.deb
+# sudo apt install -y ./balena-etcher_1.19.21_amd64.deb
+# rm balena-etcher_1.19.21_amd64.deb
+# cd -
+wget https://github.com/balena-io/etcher/releases/download/v1.18.9/balenaEtcher-1.18.9-x64.AppImage
