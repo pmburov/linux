fbb71cc9267f83ab95d0b0167309bb76218f6a30
Pavel <you@example.com>
2024-06-24 23:51:23 +0300

    f discord from deb

--- apps/discord.sh
+++ apps/discord.sh
@@ -1,6 +1,7 @@
 # --- Discord ---
-cd ~/Downloads
-wget https://dl.discordapp.net/apps/linux/0.0.55/discord-0.0.55.deb
-sudo apt install -y ./discord-0.0.55.deb
-rm discord-0.0.55.deb
-cd -
+# cd ~/Downloads
+# wget https://dl.discordapp.net/apps/linux/0.0.55/discord-0.0.55.deb
+# sudo apt install -y ./discord-0.0.55.deb
+# rm discord-0.0.55.deb
+# cd -
+flatpak install -y flathub com.discordapp.Discord
