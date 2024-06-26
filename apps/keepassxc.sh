c8fbdb3503b4ba3dd9dfa2b5d72554093e696cbf
Pavel <you@example.com>
2024-06-27 00:50:36 +0300

    obsidian flathub

--- apps/keepassxc.sh
+++ apps/keepassxc.sh
@@ -2,3 +2,4 @@
 sudo add-apt-repository -y ppa:phoerious/keepassxc
 sudo apt update
 sudo apt install -y keepassxc
+# flatpak install -y flathub org.keepassxc.KeePassXC
--- apps/obsidian.sh
+++ apps/obsidian.sh
@@ -1,7 +1,7 @@
 # --- Obsidian ---
-# flatpak install -y flathub md.obsidian.Obsidian
-cd ~/Downloads
-wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.6.2/obsidian_1.6.2_amd64.deb
-sudo apt install -y ./obsidian_1.6.2_amd64.deb
-rm obsidian_1.6.2_amd64.deb
-cd -
+flatpak install -y flathub md.obsidian.Obsidian
+# cd ~/Downloads
+# wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.6.2/obsidian_1.6.2_amd64.deb
+# sudo apt install -y ./obsidian_1.6.2_amd64.deb
+# rm obsidian_1.6.2_amd64.deb
+# cd -
