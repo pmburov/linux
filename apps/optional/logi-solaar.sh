df856936d75f93df0568ff53de93e0fe95bdb2a9
Pavel <you@example.com>
2024-06-27 21:05:16 +0300

    solaar for logi devices

---
+++ apps/optional/logi-solaar.sh
@@ -0,0 +1,5 @@
+# --- Solaar ---
+# Solaar is a Linux manager for many Logitech keyboards, mice, and other devices that connect wirelessly to a Unifying, Bolt, Lightspeed or Nano receiver as well as many Logitech devices that connect via a USB cable or Bluetooth.
+sudo add-apt-repository -y ppa:solaar-unifying/stable
+sudo apt update
+sudo apt install -y solaar
