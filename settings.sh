8b757acb2d1b3bccf7a6275679aaeead443fcb09
Pavel <you@example.com>
2024-06-28 00:23:54 +0300

    notification-banner-position

--- settings.sh
+++ settings.sh
@@ -16,7 +16,7 @@ gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,max
 dconf write /org/gnome/shell/extensions/custom-hot-corners-extended/monitor-0-bottom-left-0/action "'lock-screen'"
 dconf write /org/gnome/shell/extensions/custom-hot-corners-extended/monitor-0-top-right-0/action "'toggle-overview'"
 dconf write /org/gnome/shell/extensions/just-perfection/clock-menu-position 1
-dconf write /org/gnome/shell/extensions/just-perfection/notification-banner-position 3
+dconf write /org/gnome/shell/extensions/just-perfection/notification-banner-position 2
 dconf write /org/gnome/shell/extensions/tactile/col-3 0
 dconf write /org/gnome/shell/extensions/tactile/gap-size 4
 dconf write /org/gnome/shell/extensions/tactile/layout-2-row-2 0
