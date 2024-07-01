40b6a0aa9cc3e1100ee6728d9b392d4175988488
Pavel <you@example.com>
2024-07-01 19:39:24 +0300

    GitFiend

---
+++ apps/optional/gitfiend.sh
@@ -0,0 +1,6 @@
+# --- GitFiend ---
+cd ~/Downloads
+wget https://github.com/GitFiend/Support/releases/download/v0.44.3/GitFiend_0.44.3_amd64.deb
+sudo apt install -y ./GitFiend_0.44.3_amd64.deb
+rm GitFiend_0.44.3_amd64.deb
+cd -
