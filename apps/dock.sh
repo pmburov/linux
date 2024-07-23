5bbf6a3ad1d54d4ca915e81ede28dae0129b557c
Pavel <you@example.com>
2024-07-23 12:21:42 +0300

    minor tweaks

--- apps/dock.sh
+++ apps/dock.sh
@@ -1,2 +1,1 @@
-gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'brave-browser.desktop', 'code.desktop', 'org.gnome.Terminal.desktop', 'obsidian.desktop', 'org.keepassxc.KeePassXC.desktop', 'org.telegram.desktop.desktop', 'com.discordapp.Discord.desktop']
-"
+gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'brave-browser.desktop', 'code.desktop', 'org.gnome.Terminal.desktop', 'md.obsidian.Obsidian.desktop', 'org.keepassxc.KeePassXC.desktop', 'org.telegram.desktop.desktop', 'com.discordapp.Discord.desktop']"
