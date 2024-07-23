67be3c8266e7a11b6b0874dbcf61646a505e5e39
Pavel <you@example.com>
2024-07-23 13:49:46 +0300

    node brew, zsh

--- apps/node.sh
+++ apps/node.sh
@@ -3,4 +3,6 @@
 # source ~/.linux/configs/node-nvm-export.sh
 # nvm install 22
 # nvm use 22
-brew install node@20
+/home/linuxbrew/.linuxbrew/bin/brew install node@20
+echo 'export PATH="/home/linuxbrew/.linuxbrew/opt/node@20/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH="/home/linuxbrew/.linuxbrew/opt/node@20/bin:$PATH"' >> ~/.zshrc
--- apps/zsh.sh
+++ apps/zsh.sh
@@ -1,15 +1,9 @@
 # --- Zsh ---
 sudo apt install -y zsh
-chsh -s $(which zsh)
-
+sudo chsh -s $(which zsh)
 
 # --- Oh My Zsh ---
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# --- Oh My Zsh Theme ---
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-
 
 # Copy configs
-cp ~/.linux/configs/zsh/.p10k.zsh ~/.p10k.zsh
 cp ~/.linux/configs/zsh/.zshrc ~/.zshrc
