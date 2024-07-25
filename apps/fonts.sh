776f9c8073c4279da1677cd736adfe256f398f64
Pavel <you@example.com>
2024-07-25 11:04:40 +0300

    Meslo font

--- apps/fonts.sh
+++ apps/fonts.sh
@@ -6,6 +6,11 @@ unzip CascadiaMono.zip -d CascadiaFont
 cp CascadiaFont/*.ttf ~/.local/share/fonts
 rm -rf CascadiaMono.zip CascadiaFont
 
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip
+unzip Meslo.zip -d Meslo
+cp Meslo/*.ttf ~/.local/share/fonts
+rm -rf Meslo.zip Meslo
+
 wget -O iafonts.zip https://github.com/iaolo/iA-Fonts/archive/refs/heads/master.zip
 unzip iafonts.zip -d iaFonts
 cp iaFonts/iA-Fonts-master/iA\ Writer\ Mono/Static/iAWriterMonoS-*.ttf ~/.local/share/fonts
